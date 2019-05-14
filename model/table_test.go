26a9a4735d612147efbe0ec05563c47e54aacc3f
Роман Чалый <cerebrum.ch@gmail.com>
2019-05-14 14:41:03 +0700

    test(model/table_test): add test

---
+++ model/table_test.go
@@ -0,0 +1,141 @@
+package model
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDatabase_GetTable(t *testing.T) {
+	type args struct {
+		regionID int
+		tableID  int
+	}
+	tests := []struct {
+		name    string
+		d       *Database
+		args    args
+		want    *Table
+		wantErr error
+	}{
+		{name: "Test error", d: &Database{err: errorStub}, want: nil, wantErr: errorStub},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.d.GetTable(tt.args.regionID, tt.args.tableID)
+			if err != tt.wantErr {
+				t.Errorf("Database.GetTable() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Database.GetTable() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDatabase_GetPrivilege(t *testing.T) {
+	type args struct {
+		emailUser string
+		keyUser   string
+		idTable   int
+	}
+	tests := []struct {
+		name    string
+		d       *Database
+		args    args
+		want    bool
+		wantErr error
+	}{
+		{name: "Test error", d: &Database{err: errorStub}, want: false, wantErr: errorStub},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.d.GetPrivilege(tt.args.emailUser, tt.args.keyUser, tt.args.idTable)
+			if err != tt.wantErr {
+				t.Errorf("Database.GetPrivilege() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Database.GetPrivilege() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDatabase_GetTablesInfo(t *testing.T) {
+	tests := []struct {
+		name    string
+		d       *Database
+		want    map[int]TableInfo
+		wantErr error
+	}{
+		{name: "Test error", d: &Database{err: errorStub}, want: nil, wantErr: errorStub},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.d.GetTablesInfo()
+			if err != tt.wantErr {
+				t.Errorf("Database.GetTablesInfo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Database.GetTablesInfo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDatabase_GetHeaders(t *testing.T) {
+	type args struct {
+		idTable int
+	}
+	tests := []struct {
+		name    string
+		d       *Database
+		args    args
+		want    *Headers
+		wantErr error
+	}{
+		{name: "Test error", d: &Database{err: errorStub}, want: nil, wantErr: errorStub},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.d.GetHeaders(tt.args.idTable)
+			if err != tt.wantErr {
+				t.Errorf("Database.GetHeaders() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Database.GetHeaders() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDatabase_GetTextForEmptyTable(t *testing.T) {
+	type args struct {
+		idRegion int
+		idTable  int
+	}
+	tests := []struct {
+		name    string
+		d       *Database
+		args    args
+		want    string
+		wantErr error
+	}{
+		{name: "Test error", d: &Database{err: errorStub}, want: "", wantErr: errorStub},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.d.GetTextForEmptyTable(tt.args.idRegion, tt.args.idTable)
+			if err != tt.wantErr {
+				t.Errorf("Database.GetTextForEmptyTable() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Database.GetTextForEmptyTable() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
