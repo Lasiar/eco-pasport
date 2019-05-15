545c5b63835392924706a5fae6697e3723e7f86f
Роман Чалый <cerebrum.ch@gmail.com>
2019-05-15 11:18:07 +0700

    test(model/model_test): fill tests

--- model/model_test.go
+++ model/model_test.go
@@ -22,110 +22,47 @@ func TestDatabase_SetDB(t *testing.T) {
 	type args struct {
 		db *sql.DB
 	}
+	db, _, err := Init()
+	if err != nil {
+		t.Error(err)
+	}
+
 	tests := []struct {
 		name string
 		d    *Database
 		args args
+		want *Database
 	}{
-		// TODO: Add test cases.
+		{name: "set current db", d: db, args: args{db.db}, want: db},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.d.SetDB(tt.args.db)
 		})
-	}
-}
-
-func TestDatabase_Error(t *testing.T) {
-	tests := []struct {
-		name string
-		d    *Database
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.d.Error(); got != tt.want {
-				t.Errorf("Database.Error() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestDatabase_newDatabase(t *testing.T) {
-	tests := []struct {
-		name string
-		d    *Database
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.d.newDatabase()
-		})
+		if !reflect.DeepEqual(tt.d, tt.want) {
+			t.Errorf("Database.GetRegionInfo() got = %v, want %v", tt.d, tt.want)
+		}
 	}
 }
 
 func TestGetDatabase(t *testing.T) {
+	db, _, err := Init()
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.SetDB(db.db)
+
 	tests := []struct {
 		name string
 		want *Database
 	}{
-		// TODO: Add test cases.
+		{name: "With set current db", want: db},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetDatabase(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetDatabase() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestDatabase_connectMSSQL(t *testing.T) {
-	tests := []struct {
-		name    string
-		d       *Database
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.d.connectMSSQL(); (err != nil) != tt.wantErr {
-				t.Errorf("Database.connectMSSQL() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestDatabase_GetRegionInfo(t *testing.T) {
-	type args struct {
-		id int
-	}
-	tests := []struct {
-		name    string
-		d       *Database
-		args    args
-		want    *RegionInfo
-		want1   bool
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1, err := tt.d.GetRegionInfo(tt.args.id)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Database.GetRegionInfo() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Database.GetRegionInfo() got = %v, want %v", got, tt.want)
-			}
-			if got1 != tt.want1 {
-				t.Errorf("Database.GetRegionInfo() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
